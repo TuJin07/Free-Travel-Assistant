0a23f569714bb489da2e585f0d66ab8353deacee
x402 <akr01@qq.com>
2022-05-07 22:18:00 +0800

    照着template修改了目录

---
+++ app/src/main/java/com/example/free_travel_assistant/ui/create/CreateViewModel.java
@@ -0,0 +1,4 @@
+package com.example.free_travel_assistant.ui.create;
+
+public class CreateViewModel {
+}
---
+++ app/src/main/java/com/example/free_travel_assistant/ui/mine/MineFragment.java
@@ -0,0 +1,4 @@
+package com.example.free_travel_assistant.ui.mine;
+
+public class MineFragment {
+}
